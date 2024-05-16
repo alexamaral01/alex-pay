efb6cdccece17d4cde97127165e62f6073dae9bb
Alex Amaral <alexamaral01@gmail.com>
2024-05-15 22:00:31 -0300

    Init project

---
+++ src/main/java/com/alex/pay/AlexPayApplication.java
@@ -0,0 +1,13 @@
+package com.alex.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/pay/AlexPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
